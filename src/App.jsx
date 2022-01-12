3fd1ab99f7ceb9edab8b666f9e6f7b0e8080877f
Prathamesh <prathameshj0407@gmail.com>
2022-01-12 16:34:18 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,78 @@
+import React, { useState } from "react";
+
+const App = () => {
+  const [fullName, setfullName] = useState({
+    fname: "",
+    lname: "",
+    email: "",
+  });
+
+  const changeEvent = (event) => {
+    const value = event.target.value;
+    const name = event.target.name;
+    setfullName((preVal) => {
+      if (name === "fname") {
+        return {
+          fname: value,
+          lname: preVal.lname,
+        };
+      } else if (name === "lname") {
+        return {
+          fname: preVal.fname,
+          lname: value,
+        };
+      } else if (name === "email") {
+        return {
+          fname: preVal.fname,
+          lname: preVal.lname,
+          email: value,
+        };
+      }
+    });
+  };
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    alert("Your form has been submitted");
+  };
+  return (
+    <>
+      <div className="forms">
+        <form onSubmit={onSubmit}>
+          <div className="form">
+            <h1>
+              Hello {fullName.fname} {fullName.lname}
+            </h1>
+            <p>{fullName.email} </p>
+            <input
+              className="name"
+              type="text"
+              placeholder="Enter your Name"
+              name="fname"
+              onChange={changeEvent}
+              value={fullName.fname}
+            ></input>
+            <input
+              type="text"
+              placeholder="Enter your LastName"
+              name="lname"
+              onChange={changeEvent}
+              value={fullName.lname}
+            ></input>
+            <input
+              className="email"
+              type="email"
+              placeholder="Enter your email"
+              name="email"
+              onChange={changeEvent}
+              value={fullName.email}
+            ></input>
+            <button type="submit">Submit</button>
+          </div>
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default App;
